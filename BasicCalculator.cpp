02df0842b39436a98f22153e43e0b85811175b47
SharvariMurade16 <sharvari.murade.btech2023@sitpune.edu.in>
2024-08-08 19:27:25 +0530

    Add files via upload

---
+++ BasicCalculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a,b;
+  cout<<"Welcome to calculator"<<endl;
+  cout << "Enter first number: ";
+  cin >> a;
+  cout << "Enter second number: ";
+  cin >> b;
+    cout<<"The sum of your digits is = "<<(a+b)<<endl;
+    cout<<"The difference of your digits is = "<<(a-b)<<endl;
+    cout<<"The product of your digits is = "<<(a*b)<<endl;
+    if(b!=0){
+        cout<<"The division of your digits is = "<<(a/b)<<endl;
+    } else { 
+        cout<<"divide by 0 error";
+    }
+  return 0;
+}
---
+++ InputOutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int Number;
+    string String;
+    cout<<"Enter a number: ";
+    cin>>Number;
+    cout<<"Enter a word: ";
+    cin>>String;
+    cout<<"Your entered number= "<<Number<<endl;
+    cout<<"Your entered word= "<<String<<endl;
+    return 0;
+}
